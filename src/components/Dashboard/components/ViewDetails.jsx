1d3d3f5a6c1520729e565beb5380b7c02fa0323f
Nlucky320 <luckyes144@gmail.com>
2024-06-04 00:20:43 +0600

    task details page updated

--- src/components/Dashboard/components/ViewDetails.jsx
+++ src/components/Dashboard/components/ViewDetails.jsx
@@ -4,6 +4,7 @@ import useAuth from "../../../Hooks/useAuth";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import LoadingSpinner from "../../Spinner/LoadingSpinner";
 
 const ViewDetails = () => {
   const { user } = useAuth();
@@ -42,6 +43,40 @@ const ViewDetails = () => {
     const formattedDate = now.toLocaleDateString(); // You can customize the format as needed
     setCurrentDate(formattedDate);
   }, []);
+    
+     const handleSubmission = async (event) => {
+    event.preventDefault();
+    const submissionDetail = event.target.submission_detail.value;
+
+    const submissionData = {
+     
+      task_id: item?._id,
+        task_title: item?.title,
+        task_detail:item?.detail,
+        task_image: item?.image,
+        payable_amount:item?.amount,
+       submission_detail: submissionDetail,
+      worker_email: worker?.email,
+      worker_name: worker?.name,
+      taskCreator_email: item?.taskCreator?.email,
+      taskCreator_name: item?.taskCreator?.name,
+        current_date: currentDate,
+      status:'pending',
+    };
+
+    try {
+      const response = await axiosSecure.post('/submission', submissionData);
+      if (response.status === 200) {
+        console.log('Submission successful', response.data);
+        // Handle success (e.g., show a success message or redirect)
+      }
+    } catch (error) {
+      console.error('Submission failed', error);
+      // Handle error (e.g., show an error message)
+    }
+  };
+
+    if(isLoading) return <LoadingSpinner></LoadingSpinner>
   return (
     <div className="w-full min-h-[calc(100vh-400px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
       <Helmet>
@@ -98,7 +133,7 @@ const ViewDetails = () => {
             </p>
           </div>
         </div>
-        <form className="mt-4">
+        <form className="mt-4"  onSubmit={handleSubmission}>
           <label className="label">
             <span className="label-text">Submission Detail</span>
           </label>
@@ -107,7 +142,8 @@ const ViewDetails = () => {
               type="text"
               name="submission_detail"
               placeholder="Submission Detail"
-              className="input input-bordered w-full"
+                          className="input input-bordered w-full"
+                          required
             />
                   </label>
                    <input
