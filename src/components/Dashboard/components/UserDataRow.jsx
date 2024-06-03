02c5dd2843953c194c0e7b361d3e05829120e4cf
Nlucky320 <luckyes144@gmail.com>
2024-06-03 18:44:49 +0600

    user delete action added

--- src/components/Dashboard/components/UserDataRow.jsx
+++ src/components/Dashboard/components/UserDataRow.jsx
@@ -7,10 +7,10 @@ import toast from 'react-hot-toast'
 import useAuth from '../../../Hooks/useAuth'
 import useAxiosSecure from '../../../Hooks/useAxiosSecure'
 import UpdateUserModal from './UpdateUserModal'
+import Swal from 'sweetalert2'
 
 const UserDataRow = ({ user, refetch }) => {
   const { user: loggedInUser } = useAuth()
-
   const [isOpen, setIsOpen] = useState(false)
   const axiosSecure = useAxiosSecure()
   const { mutateAsync } = useMutation({
@@ -46,7 +46,35 @@ const UserDataRow = ({ user, refetch }) => {
       console.log(err)
       toast.error(err.message)
     }
-  }
+    }
+    
+  const deleteMutation = useMutation({
+    mutationFn: (user) => axiosSecure.delete(`/users/${user?.email}`),
+    onSuccess: () => {
+      Swal.fire("Deleted!", "User has been deleted.", "success");
+      refetch();
+    },
+    onError: (error) => {
+      console.error("Error deleting user:", error);
+    },
+  });
+
+  const handleDelete = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteMutation.mutate(user);
+      }
+    });
+  };
+
   return (
     <tr>
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
@@ -82,7 +110,13 @@ const UserDataRow = ({ user, refetch }) => {
         />
           </td>
           <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-       <p className='text-gray-900 whitespace-no-wrap'>Remove</p>
+      <button onClick={() => handleDelete(user)}
+                    type='button'
+                    className='inline-flex justify-center rounded-full border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
+            
+                  >
+                  Remove
+                  </button>
       </td>
     </tr>
   )
