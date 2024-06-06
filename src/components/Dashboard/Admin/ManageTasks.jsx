7a865ebafc0fbdc5ef90da1bc2cb0bc26d01fcdf
Nlucky320 <luckyes144@gmail.com>
2024-06-06 21:53:45 +0600

    home updated

--- src/components/Dashboard/Admin/ManageTasks.jsx
+++ src/components/Dashboard/Admin/ManageTasks.jsx
@@ -6,7 +6,7 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import Swal from "sweetalert2";
 import LoadingSpinner from "../../Spinner/LoadingSpinner";
-import { FaEdit, FaEye } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
 import { MdClose, MdDelete } from "react-icons/md";
 import Modal from "react-modal";
 
@@ -20,27 +20,24 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
-// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
-Modal.setAppElement('#root');
-
-
+Modal.setAppElement("#root");
 
 const ManageTasks = () => {
   let subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   function openModal() {
     setIsOpen(true);
   }
 
   function afterOpenModal() {
-    // references are now sync'd and can be accessed.
     subtitle.style.color = "#f00";
   }
 
   function closeModal() {
     setIsOpen(false);
   }
+
   const [items, setItems] = useState([]);
 
   const axiosSecure = useAxiosSecure();
@@ -56,7 +53,7 @@ const ManageTasks = () => {
       return response.data;
     },
   });
-  // console.log(fetchedItems)
+
   const deleteMutation = useMutation({
     mutationFn: (_id) => axiosPublic.delete(`/tasks/${_id}`),
     onSuccess: () => {
@@ -65,6 +62,7 @@ const ManageTasks = () => {
     },
     onError: (error) => {
       console.error("Error deleting task:", error);
+      Swal.fire("Error", "Task deletion failed", "error");
     },
   });
 
@@ -74,7 +72,7 @@ const ManageTasks = () => {
       text: "You won't be able to revert this!",
       icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: "##0044BC",
+      confirmButtonColor: "#0044BC",
       cancelButtonColor: "#d33",
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
@@ -90,11 +88,11 @@ const ManageTasks = () => {
     }
   }, [fetchedItems]);
 
-  if (isLoading) return <LoadingSpinner></LoadingSpinner>;
+  if (isLoading) return <LoadingSpinner />;
   return (
     <div>
       <Helmet>
-        <title> Manage Tasks | Dashboard</title>
+        <title>Manage Tasks | Dashboard</title>
       </Helmet>
       <div className="mx-auto text-center space-y-4">
         <h2 className="pt-6 text-2xl lg:text-4xl font-bold text-black">
@@ -121,28 +119,30 @@ const ManageTasks = () => {
                   <td>{task?.title}</td>
                   <td>{task?.taskCreator?.name}</td>
                   <td>{task?.quantity}</td>
-                  <td>{task?.amount*task?.quantity}</td>
-                  <td>{task?.quantity > 0 ? 'Available' : 'Not Available'}</td>
+                  <td>{task?.amount * task?.quantity}</td>
+                  <td>{task?.quantity > 0 ? "Available" : "Not Available"}</td>
                   <td>
-                   
+                    <button
+                      onClick={openModal}
+                      className="text-[#0044BC] bg-transparent text-2xl"
+                    >
+                      <FaEye />
+                    </button>
+                    <Modal
+                      isOpen={modalIsOpen}
+                      onAfterOpen={afterOpenModal}
+                      onRequestClose={closeModal}
+                      style={customStyles}
+                      contentLabel="Example Modal"
+                    >
                       <button
-                        onClick={openModal}
-                        className="text-[#0044BC] bg-transparent text-2xl"
+                        className="absolute top-0 right-0 bg-blue-100 p-2 text-sm font-medium rounded-full text-blue-900 hover:text-blue-700 hover:bg-blue-500 focus:outline-none"
+                        onClick={closeModal}
                       >
-                        <FaEye />
+                        <MdClose />
                       </button>
-                      <Modal
-                        isOpen={modalIsOpen}
-                        onAfterOpen={afterOpenModal}
-                        onRequestClose={closeModal}
-                        style={customStyles}
-                        contentLabel="Example Modal"
-                      >
-                        
-                        <button className="absolute top-0 right-0 bg-blue-100 p-2 text-sm font-medium rounded-full text-blue-900 Z-10hover:text-blue-700 hover:bg-blue-500 focus:outline-none" onClick={closeModal}><MdClose></MdClose> </button>
-                        <div className="p-4">Task Detail: {task.detail}</div>
-                      </Modal>
-                    
+                      <div className="p-4">Task Detail: {task.detail}</div>
+                    </Modal>
                   </td>
                   <td>
                     <button
